d7f7512a78091d081721c2f91ddae8024f8789ba
carth <me@carth.xyz>
2025-01-19 09:03:55 +0100

    update 1.35

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -26,10 +26,9 @@ backend.add(import('@backstage/plugin-events-backend'));
 
 // auth plugin
 backend.add(import('@backstage/plugin-auth-backend'));
-// See https://backstage.io/docs/backend-system/building-backends/migrating#the-auth-plugin
-backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
-// See https://backstage.io/docs/auth/guest/provider
 backend.add(import('@backstage/plugin-auth-backend-module-microsoft-provider'));
+// See https://backstage.io/docs/auth/guest/provider
+backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
 
 // catalog plugin
 backend.add(import('@backstage/plugin-catalog-backend'));
