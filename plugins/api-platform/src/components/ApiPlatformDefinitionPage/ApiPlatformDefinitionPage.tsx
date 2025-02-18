adbcaaf4bd9ffef2d4260dbfcc7c573485013f40
carth <me@carth.xyz>
2025-02-18 06:46:43 +0100

    api-platform: link api version

--- plugins/api-platform/src/components/ApiPlatformDefinitionPage/ApiPlatformDefinitionPage.tsx
+++ plugins/api-platform/src/components/ApiPlatformDefinitionPage/ApiPlatformDefinitionPage.tsx
@@ -8,42 +8,51 @@ import {
 } from '@backstage/core-components';
 import { configApiRef, useApi, useRouteRefParams } from '@backstage/core-plugin-api';
 import { entityRouteRef } from '@backstage/plugin-catalog-react';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useGetApiVersions } from '../../hooks';
 import { catalogApiRef } from '@backstage/plugin-catalog-react';
 import { ApiEntity } from '@backstage/catalog-model';
 import { ApiPlatformDefinitionCard } from './ApiPlatformDefinitionCard';
 import { Box } from '@material-ui/core';
+import { useSearchParams } from 'react-router-dom';
 
 export const ApiPlatformDefinitionPage = () => {
   const { name } = useRouteRefParams(entityRouteRef);
+  const [searchParams] = useSearchParams();
+  const queryVersion = searchParams.get('version');
+
   const { apiVersions, loading, error } = useGetApiVersions(name);
   const catalogApi = useApi(catalogApiRef);
 
-  const [selectedVersion, setSelectedVersion] = useState<string>("");
   const [versions, setVersions] = useState<SelectItem[]>([]);
+  const [selectedVersion, setSelectedVersion] = useState<string | undefined>(undefined);
   const [apiEntity, setApiEntity] = useState<ApiEntity | undefined>(undefined);
 
-  useEffect(() => {
-    if (versions.length > 0) {
-      setSelectedVersion(versions.at(0)?.value.toString()!);
-    }
-  }, [versions]);
+  const isInitialLoad = useRef(true);
+
+  const configApi = useApi(configApiRef);
 
   useEffect(() => {
-    if (selectedVersion !== "") {
+    const data = apiVersions
+    ? apiVersions.map(apiVersion => ({ label: apiVersion.version, value: apiVersion.entityRef }))
+    : []
+    setVersions(data);      
+    let selVersion = null;
+    if (isInitialLoad.current && queryVersion && data.some(item => item.label === queryVersion)) {
+      selVersion = data.find(item => item.label === queryVersion)?.value;
+    } else if (data.length > 0) {
+      selVersion = data[0].value;
+    }
+    if (selVersion)
+      setSelectedVersion(selVersion);
+  }, [apiVersions, queryVersion])
+  
+  useEffect(() => {
+    if (selectedVersion) {
       catalogApi.getEntityByRef(selectedVersion)
         .then(entity => setApiEntity(entity as ApiEntity));
     }
   }, [selectedVersion, catalogApi]);
-
-  useEffect(() => {
-    setVersions(apiVersions
-      ? apiVersions.map(apiVersion => ({ label: apiVersion.version, value: apiVersion.entityRef }))
-      : []);
-  }, [apiVersions])
-
-  const configApi = useApi(configApiRef);
   const generatedSubtitle = `${configApi.getOptionalString('organization.name') ?? 'Backstage'} API Explorer`;
 
   if (error) {
