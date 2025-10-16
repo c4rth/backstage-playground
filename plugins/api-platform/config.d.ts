5b3f5309f7b862d93fac5ccbe7b7e233d14c2e3e
carth <me@carth.xyz>
2025-10-16 06:59:18 +0200

    update deps

--- plugins/api-platform/config.d.ts
+++ plugins/api-platform/config.d.ts
@@ -10,5 +10,27 @@ export interface Config {
     * @visibility frontend
      */
     organization: string;
-  };
+  },
+  apiPlatform?: {
+    /**
+     * The organization of the given Azure instance
+    * @visibility frontend
+     */
+    organization: string;
+    /**
+     * The feed name of the given Azure instance
+    * @visibility frontend
+     */
+    feedName: string;
+    /**
+     * The DNS name of the API Platform instance
+    * @visibility frontend
+     */
+    dns: string;
+    /**
+     * The group prefix for APIs in the API Platform instance
+    * @visibility frontend
+     */
+    groupPrefix: string;
+  }
 }
